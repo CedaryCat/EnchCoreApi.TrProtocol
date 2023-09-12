51147560b5b154d87ae6b2dcc1f83d8ed06de4de
纸鸢 <2503439479@qq.com>
2023-09-12 17:29:28 +0800

    feat: add enum TileEditAction for TileChange packet

---
+++ src/EnchCoreApi.TrProtocol/Models/TileEditAction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnchCoreApi.TrProtocol.Models
+{
+    public enum TileEditAction : byte
+    {
+        KillTile = 0,
+        PlaceTile,
+        KillWall,
+        PlaceWall,
+        KillTileNoItem,
+        PlaceWire,
+        KillWire,
+        PoundTile,
+        PlaceActuator,
+        KillActuator,
+        PlaceWire2,
+        KillWire2,
+        PlaceWire3,
+        KillWire3,
+        SlopeTile,
+        FrameTrack,
+        PlaceWire4,
+        KillWire4,
+        PokeLogicGate,
+        Acutate,
+        TryKillTile,
+        ReplaceTile,
+        ReplaceWall,
+        SlopePoundTile
+    }
+}
--- src/EnchCoreApi.TrProtocol/NetPackets/TileChange.cs
+++ src/EnchCoreApi.TrProtocol/NetPackets/TileChange.cs
@@ -1,10 +1,11 @@
-﻿using Terraria.DataStructures;
+﻿using EnchCoreApi.TrProtocol.Models;
+using Terraria.DataStructures;
 
 namespace EnchCoreApi.TrProtocol.NetPackets;
 
 public partial class TileChange : NetPacket {
     public sealed override MessageID Type => MessageID.TileChange;
-    public byte ChangeType;
+    public TileEditAction ChangeType;
     public Point16 Position;
     public short TileType;
     public byte Style;
